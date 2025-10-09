391379376af2fd9c2127e62e24124b12de58df54
Aya Helal <aya.helal.2004@gmail.com>
2025-10-09 17:40:24 +0300

    create meetza project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <Router>
+
+    </Router>
+  );
+}
+
+export default App;
