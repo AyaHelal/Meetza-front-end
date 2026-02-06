19104c0944c833a0a128d53cfba32b0844e41839
Aya Helal <aya.helal.2004@gmail.com>
2026-02-06 16:30:39 +0200

    create meeting room

--- src/pages/Meetings/Meetings.js
+++ src/pages/Meetings/Meetings.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
+import MeetingRoom from "./components/MeetingRoom";
+import "./Meetings.css";
 
 const Meetings = () => {
     return (
-        <div>
-            Meetings Page
+        <div className="meetings-container">
+        {/* Center: meeting room card + empty comment area */}
+        <div className="meetings-center">
+            <MeetingRoom />
+            {/* Empty place for comments */}
+            <div className="meetings-comments-placeholder">
+            <h4 className="meetings-comments-title">Comments during meeting</h4>
+            </div>
         </div>
-    )
-}
 
-export default Meetings
+        {/* Right: empty space for sidebar */}
+        <div className="meetings-right-placeholder" aria-hidden="true" />
+        </div>
+    );
+};
 
+export default Meetings;
